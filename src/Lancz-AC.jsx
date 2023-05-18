80cfe5274cbc04338fbdebfe85bc3fc24653e368
rajkaiandras <rajkaiandras@gmail.com>
2023-05-18 02:04:55 +0200

    init Lancz Air Conditioning repo

---
+++ src/Lancz-AC.jsx
@@ -0,0 +1,13 @@
+// Styles
+import './Lancz-AC.css';
+
+// Components
+import Header from './components/Header/Header.jsx';
+
+export default function LanczAC() {
+  return (
+    <div className="Lancz-AC">
+      <Header />
+    </div>
+  );
+}
