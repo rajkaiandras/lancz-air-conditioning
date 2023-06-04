39d62ffdb1a8b4d06c4d8e00508e5fb766cc0a57
rajkaiandras <rajkaiandras@gmail.com>
2023-06-04 09:02:58 +0200

    Price offer modified grid layout!

--- src/components/PriceOfferForm/PriceOfferForm.jsx
+++ src/components/PriceOfferForm/PriceOfferForm.jsx
@@ -10,7 +10,7 @@ export const PriceOfferForm = () => {
   const [offerSubject, setOfferSubject] = useState('');
   const [offerInformation, setOfferInformation] = useState('');
 
-  const priceOffer = {
+  const priceOfferData = {
     'Teljes név': fullName,
     Telefonszám: phoneNumber,
     'E-mail cím': emailAddress,
@@ -20,57 +20,56 @@ export const PriceOfferForm = () => {
 
   const handlePriceOfferSubmit = (e) => {
     e.preventDefault();
-    console.log(priceOffer);
+    console.log(priceOfferData);
   };
 
   return (
     <form className="PriceOfferForm">
-      <div className="input-container">
-        <input
-          type="text"
-          value={fullName}
-          onChange={(e) => setFullName(e.target.value)}
-          placeholder="Teljes név"
-        />
-        <input
-          type="tel"
-          value={phoneNumber}
-          onChange={(e) => setPhoneNumber(e.target.value)}
-          placeholder="Telefonszám"
-        />
-      </div>
-      <div className="input-container">
-        <input
-          type="email"
-          value={emailAddress}
-          onChange={(e) => setEmailAddress(e.target.value)}
-          placeholder="E-mail"
-        />
-        <select
-          value={offerSubject}
-          onChange={(e) => setOfferSubject(e.target.value)}
-        >
-          <option value="install">Telepítés</option>
-          <option value="repair">Javítás</option>
-          <option value="warrantee">Garancia</option>
-        </select>
-      </div>
-      <div className="input-container">
-        <textarea
-          type="text"
-          value={offerInformation}
-          onChange={(e) => setOfferInformation(e.target.value)}
-          placeholder="Információk"
-          /* rows="4"
+      <input
+        className="input--full-name"
+        type="text"
+        value={fullName}
+        onChange={(e) => setFullName(e.target.value)}
+        placeholder="Teljes név"
+      />
+      <input
+        className="input--phone-number"
+        type="tel"
+        value={phoneNumber}
+        onChange={(e) => setPhoneNumber(e.target.value)}
+        placeholder="Telefonszám"
+      />
+      <input
+        className="input--e-mail"
+        type="email"
+        value={emailAddress}
+        onChange={(e) => setEmailAddress(e.target.value)}
+        placeholder="E-mail"
+      />
+      <select
+        className="input--offer-subject"
+        value={offerSubject}
+        onChange={(e) => setOfferSubject(e.target.value)}
+      >
+        <option value="install">Telepítés</option>
+        <option value="repair">Javítás</option>
+        <option value="warrantee">Garancia</option>
+      </select>
+      <textarea
+        className="input--offer-information"
+        type="text"
+        value={offerInformation}
+        onChange={(e) => setOfferInformation(e.target.value)}
+        placeholder="Információk"
+        /* rows="4"
         cols="50" */
-        />
-        <input
-          className="btn btn--filled"
-          type="submit"
-          onClick={handlePriceOfferSubmit}
-          value="Elküldés"
-        />
-      </div>
+      />
+      <input
+        className="btn btn--filled input--submit"
+        type="submit"
+        onClick={handlePriceOfferSubmit}
+        value="Elküldés"
+      />
     </form>
   );
 };
