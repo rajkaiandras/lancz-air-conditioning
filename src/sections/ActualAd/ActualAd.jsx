c4b745fa8ec4b3af7a1d4c7dfbfb942dae430cb6
rajkaiandras <rajkaiandras@gmail.com>
2023-07-18 21:33:21 +0200

    updated ac ad

--- src/sections/ActualAd/ActualAd.jsx
+++ src/sections/ActualAd/ActualAd.jsx
@@ -5,7 +5,7 @@ import { DarkModeContext } from '../../contexts/DarkModeContext.jsx';
 
 // assets
 import logoDiscount from '../../assets/images/actual-ad-logo-discount.png';
-import actualAdImg from '../../assets/images/actual-ad-gree-ac-comfort-x.png';
+import actualAdImg from '../../assets/images/actual-ad-midea-ac.png';
 
 // styles
 import './ActualAd.css';
@@ -23,20 +23,21 @@ export const ActualAd = () => {
             alt="discount logo"
           />
           <h3 className={`discount-text__title ${darkMode ? 'dark-mode' : ''}`}>
-            Aktuális Gree klíma akciónk<br></br>
-            {/* <span className="highlighted">beszereléssel együtt</span> */}
-            {
-              <span className="highlighted">
-                WiFi / 3D légáram / Cold Plasma / Távirányítóba integrált hőmérő
-              </span>
-            }
+            Aktuális Midea klíma akciónk<br></br>
+            {/* <span className="highlighted">beszereléssel együtt</span>
+            <span className="highlighted">
+              WiFi / 3D légáram / Cold Plasma / Távirányítóba integrált hőmérő
+            </span> */}
           </h3>
-          <h3 className={`discount-text__price ${darkMode ? 'dark-mode' : ''}`}>
-            Comfort X 2.5kw 253.400 Ft
-          </h3>
-          <h3 className={`discount-text__price ${darkMode ? 'dark-mode' : ''}`}>
-            Comfort X 3.5kw 262.400 Ft
-          </h3>
+          <h4 className={`discount-text__price ${darkMode ? 'dark-mode' : ''}`}>
+            Xtreme Save Pro 3.5kw 264.070 Ft
+          </h4>
+          <h4 className={`discount-text__price ${darkMode ? 'dark-mode' : ''}`}>
+            Xtreme Save Pro 2.5kw 254.700 Ft
+          </h4>
+          <h4 className={`discount-text__price ${darkMode ? 'dark-mode' : ''}`}>
+            Vivax Cool (Midea) 156.400 Ft
+          </h4>
           <div
             className={`frame-decoration ${darkMode ? 'dark-mode' : ''}`}
           ></div>
